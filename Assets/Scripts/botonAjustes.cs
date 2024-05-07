36d98629416a03de55bdaef17cfcefac46efc4f7
DanielZhu26 <125443437+DanielZhu26@users.noreply.github.com>
2024-05-07 19:02:30 +0200

    menu de ajustes terminado

---
+++ Assets/Scripts/botonAjustes.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class botonAjustes : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        UIDocument uiDoc = GetComponent<UIDocument>();
+        VisualElement rootVe = uiDoc.rootVisualElement;
+
+        UQueryBuilder<VisualElement> builder = new(rootVe);
+        List<VisualElement> lista_ve = builder.ToList();
+
+        VisualElement boton1 = rootVe.Q<Button>("botonMusic");
+        VisualElement boton2 = rootVe.Q<Button>("botonOffers");
+        VisualElement boton3 = rootVe.Q<Button>("botonSfx");
+        VisualElement boton4 = rootVe.Q<Button>("botonChat");
+        VisualElement boton5 = rootVe.Q<Button>("botonLanguage");
+        VisualElement boton6 = rootVe.Q<Button>("botonFace");
+
+
+        boton1.RegisterCallback<MouseUpEvent>(ev =>
+        {
+
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (buttonVe.style.backgroundColor == Color.red)
+            {
+                buttonVe.style.backgroundColor = Color.green;
+                rootVe.Q<Button>("botonMusic").text = "ON"; 
+            }
+            else
+            {
+                buttonVe.style.backgroundColor = Color.red;
+                rootVe.Q<Button>("botonMusic").text = "OFF";
+            }
+        });
+
+        boton2.RegisterCallback<MouseUpEvent>(ev =>
+        {
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (buttonVe.style.backgroundColor == Color.red)
+            {
+                buttonVe.style.backgroundColor = Color.green;
+                rootVe.Q<Button>("botonOffers").text = "ON";
+            }
+            else
+            {
+                buttonVe.style.backgroundColor = Color.red;
+                rootVe.Q<Button>("botonOffers").text = "OFF";
+            }
+        });
+
+        boton3.RegisterCallback<MouseUpEvent>(ev =>
+        {
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (buttonVe.style.backgroundColor == Color.red)
+            {
+                buttonVe.style.backgroundColor = Color.green;
+                rootVe.Q<Button>("botonSfx").text = "ON";
+            }
+            else
+            {
+                buttonVe.style.backgroundColor = Color.red;
+                rootVe.Q<Button>("botonSfx").text = "OFF";
+            }
+        });
+
+        boton4.RegisterCallback<MouseUpEvent>(ev =>
+        {
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (buttonVe.style.backgroundColor == Color.red)
+            {
+                buttonVe.style.backgroundColor = Color.green;
+                rootVe.Q<Button>("botonChat").text = "ON";
+            }
+            else
+            {
+                buttonVe.style.backgroundColor = Color.red;
+                rootVe.Q<Button>("botonChat").text = "OFF";
+            }
+        });
+
+        boton5.RegisterCallback<MouseUpEvent>(ev =>
+        {
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (rootVe.Q<Button>("botonLanguage").text == "ENGLISH")
+            {
+             
+                rootVe.Q<Button>("botonLanguage").text = "SPANISH";
+            }
+            else
+            {
+           
+                rootVe.Q<Button>("botonLanguage").text = "ENGLISH";
+            }
+        });
+
+        boton6.RegisterCallback<MouseUpEvent>(ev =>
+        {
+            VisualElement buttonVe = (ev.target as VisualElement);
+            if (buttonVe.style.backgroundColor == Color.red)
+            {
+                buttonVe.style.backgroundColor = Color.green;
+                rootVe.Q<Button>("botonFace").text = "CONNECTED";
+            }
+            else
+            {
+                buttonVe.style.backgroundColor = Color.red;
+                rootVe.Q<Button>("botonFace").text = "DISCONNECTED";
+            }
+        });
+
+    }
+}
